c0a1e802ed7d4b193d8a107df57dda973646db29
bboyjing <bboyjing@gmail.com>
2019-03-08 17:34:03 +0800

    remove deployContract

--- library/template.sol
+++ library/template.sol
@@ -41,56 +41,5 @@ contract Template {
     function getTemplateInfo() public view returns (uint16, string){
         return (category, templateName);
     }
-    
-    /// @dev deploy contract 
-    function deployContract(uint16 _category, string _templateName, bytes _constructorParam) public returns (address) {
-        bytes memory byteCode;
-        uint16 status;
-        
-        (,byteCode,,,,,status) = templateWarehouse.getTemplate(_category, _templateName);
-        // make sure template is approved
-        require(status == 1);
-        
-        // calculate input
-        bytes memory input;
-         if (_constructorParam.length > 0) {
-            input = mergeBytes(byteCode, _constructorParam);
-        } else {
-            input = byteCode;
-        }
-        
-        address deployedAddress;
-        assembly {
-          deployedAddress := create(0, add(input, 0x20), mload(input))
-        }
-        
-        require(deployedAddress != 0x0000000000000000000000000000000000000000000000000000000000000000);
-        Template t = Template(deployedAddress);
-        t.initTemplateExternal(_category, _templateName);
-        emit RecordAddress(deployedAddress);
-        return deployedAddress; 
-    }
-    
-    function mergeBytes(bytes memory a, bytes memory b) internal pure returns (bytes memory c) {
-        // Store the length of the first array
-        uint alen = a.length;
-        // Store the length of BOTH arrays
-        uint totallen = alen + b.length;
-        // Count the loops required for array a (sets of 32 bytes)
-        uint loopsa = (a.length + 31) / 32;
-        // Count the loops required for array b (sets of 32 bytes)
-        uint loopsb = (b.length + 31) / 32;
-        assembly {
-            let m := mload(0x40)
-            // Load the length of both arrays to the head of the new bytes array
-            mstore(m, totallen)
-            // Add the contents of a to the array
-            for {  let i := 0 } lt(i, loopsa) { i := add(1, i) } { mstore(add(m, mul(32, add(1, i))), mload(add(a, mul(32, add(1, i))))) }
-            // Add the contents of b to the array
-            for {  let i := 0 } lt(i, loopsb) { i := add(1, i) } { mstore(add(m, add(mul(32, add(1, i)), alen)), mload(add(b, mul(32, add(1, i))))) }
-            mstore(0x40, add(m, add(32, totallen)))
-            c := m
-        }
-    }
 
 }
