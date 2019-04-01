b20929281b318d79a1218561f6e3a938bb6de1b1
seeplayerone <38274431+seeplayerone@users.noreply.github.com>
2019-04-01 20:41:33 +0800

    Update template.sol

--- library/template.sol
+++ library/template.sol
@@ -18,7 +18,7 @@ contract Template {
     ///  it was originally the logic inside the constructor
     ///  it is changed in such way to provide a better user experience in the Flow debugging tool
     function initTemplate(uint16 _category, string _templateName) public {
-        // require(msg.sender == 0x66dbdd2826fb068f2929af065b04c0804d0397b09e);
+        require(msg.sender == 0x66dbdd2826fb068f2929af065b04c0804d0397b09e);
         category = _category;
         templateName = _templateName;
     }
