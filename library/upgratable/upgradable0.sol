a7be8299be8a6777590f3100ed6adee78442ecf2
bboyjing <bboyjing@gmail.com>
2019-06-04 10:45:05 +0800

    move upgradable0

---
+++ library/upgratable/upgradable0.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.4.25;
+
+contract Upgradable0 {
+    
+    string name;
+    
+    function setName(string _name) public {
+        name = _name;
+    }
+    
+    function getName() public returns (string) {
+        return name;
+    } 
+    
+}
