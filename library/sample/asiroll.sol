f60ab5c9b1b09a297deb11534f26856e3bbcb593
jackyxxu <xxd@ipaitao.com>
2019-07-12 13:43:16 +0800

    update asiroll

--- library/sample/asiroll.sol
+++ library/sample/asiroll.sol
@@ -11,32 +11,59 @@ import "github.com/seeplayerone/dapp-bin/library/template.sol";
 
 contract AsiRoll is ACL, Template {
 
+    string constant FALLBACK = "FALLBACK";
     string constant DEPOSIT = "DEPOSIT";
 
+    uint private balance;
+    uint private roll;
+    uint private win;
+
     constructor() public payable {
+        balance = SafeMath.add(balance, msg.value);
+        configureFunctionAddressInternal(FALLBACK, msg.sender, OpMode.Add);
         configureFunctionAddressInternal(DEPOSIT, msg.sender, OpMode.Add);
     }
 
-    function() public payable{}
-
-    function deposit() public payable authFunctionHash(DEPOSIT) {
+    function() public payable authFunctionHash(FALLBACK) {
+        balance = SafeMath.add(balance, msg.value);
     }
 
-    function roll() public payable {
+    function deposit() public payable authFunctionHash(DEPOSIT) {
+        balance = SafeMath.add(balance, msg.value);
+    }
+
+    function play(uint lucky) public payable {
+        require(lucky > 0);
+        require(lucky < 100);
         uint asset = msg.assettype;
         uint value = msg.value;
-        if(rollInternal() == 0) {
-            msg.sender.transfer(SafeMath.mul(value, 2), asset);
+
+        roll = random();
+
+        /// u win
+        if(lucky >= roll) {
+            win = SafeMath.div(SafeMath.mul(value, 100), lucky);
+            msg.sender.transfer(win, asset);
+            balance = SafeMath.sub(balance, win);
+        } else {
+            win = 0;
         }
     }
 
-    function rollInternal() internal view returns (uint){
-        uint seed = block.timestamp;
-        return SafeMath.mod(seed, 2);
+    function random() private view returns (uint) {
+        return SafeMath.mod(uint(keccak256(block.timestamp)),100);
     }
 
     function getBalance() public view returns (uint) {
-        return address(this).balance;
+        return balance;
+    }
+
+    function getLastRoll() public view returns (uint) {
+        return roll;
+    }
+
+    function getLastWin() public view returns (uint) {
+        return win;
     }
 
 
