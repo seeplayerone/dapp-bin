2db6df68e4a484b1a634a5fc2954ff70d73970fb
jackyxxu <xxd@ipaitao.com>
2019-07-12 16:33:02 +0800

    update asiroll

--- library/sample/asiroll.sol
+++ library/sample/asiroll.sol
@@ -9,42 +9,70 @@ import "github.com/seeplayerone/dapp-bin/library/acl.sol";
 import "github.com/seeplayerone/dapp-bin/library/template.sol";
 
 
+/// @dev this is a sample roll contract
+///  DO NOT TAKE IT SERIOUSLY !!!
+
 contract AsiRoll is ACL, Template {
 
-    string constant FALLBACK = "FALLBACK";
-    string constant DEPOSIT = "DEPOSIT";
+    string constant ADMIN_FUNCTIONS = "ADMIN_FUNCTIONS";
 
     uint private balance;
     uint private roll;
     uint private win;
+    uint private lucky;
+
+    uint96 constant ASIM = 0x000000000000; 
 
     constructor() public payable {
-        balance = SafeMath.add(balance, msg.value);
-        configureFunctionAddressInternal(FALLBACK, msg.sender, OpMode.Add);
-        configureFunctionAddressInternal(DEPOSIT, msg.sender, OpMode.Add);
+        configureFunctionAddressInternal(ADMIN_FUNCTIONS, msg.sender, OpMode.Add);
+        deposit();
     }
 
-    function() public payable authFunctionHash(FALLBACK) {
+    function() public payable authFunctionHash(ADMIN_FUNCTIONS) {
+        deposit();
+    }
+
+    function deposit() public payable authFunctionHash(ADMIN_FUNCTIONS) {
+        if(msg.value > 0) {
+            require(msg.assettype == ASIM);
+        }
         balance = SafeMath.add(balance, msg.value);
     }
 
-    function deposit() public payable authFunctionHash(DEPOSIT) {
-        balance = SafeMath.add(balance, msg.value);
+    function withdraw() public authFunctionHash(ADMIN_FUNCTIONS) {
+        require(balance > 0);
+        msg.sender.transfer(balance, ASIM);
+        balance = 0;       
     }
 
-    function play(uint lucky) public payable {
+    function play(uint emm) public payable {
+        lucky = emm;
+
         require(lucky > 0);
-        require(lucky < 100);
+        require(lucky < 101);
+        
         uint asset = msg.assettype;
         uint value = msg.value;
+
+        require(balance > 0);
+        if(value > 0) {
+            require(msg.assettype == ASIM);            
+        }
+
+        balance = SafeMath.add(balance, msg.value);
 
         roll = random();
 
         /// u win
-        if(lucky >= roll) {
+        if(lucky > roll) {
             win = SafeMath.div(SafeMath.mul(value, 100), lucky);
-            msg.sender.transfer(win, asset);
-            balance = SafeMath.sub(balance, win);
+            if(win < balance) {            
+                msg.sender.transfer(win, asset);
+                balance = SafeMath.sub(balance, win);
+            } else {
+                msg.sender.transfer(balance, asset);
+                balance = 0;
+            }
         } else {
             win = 0;
         }
@@ -66,5 +94,7 @@ contract AsiRoll is ACL, Template {
         return win;
     }
 
-
+    function getLastLucky() public view returns (uint) {
+        return lucky;
+    }
 }
