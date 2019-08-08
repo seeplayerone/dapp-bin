c9cdd871826dc75a444615b9598f8ab3dd1e39c9
jackyxxu <xxd@ipaitao.com>
2019-08-08 20:25:28 +0800

    add testcases

---
+++ pai-experimental/testcase/pai_issuer.tc.sol
@@ -0,0 +1,35 @@
+pragma solidity 0.4.25;
+
+import "../../library/template.sol";
+import "../pai_issuer.sol";
+import "../3rd/test.sol";
+
+// import "github.com/seeplayerone/dapp-bin/library/template.sol";
+// import "github.com/seeplayerone/dapp-bin/pai-experimental/pai_issuer.sol"
+// import "github.com/seeplayerone/dapp-bin/pai-experimental/3rd/test.sol";
+
+contract PAIIssuerTest is Template, DSTest {
+    PAIIssuer private issuer;
+
+    address private dest = 0x668eb397ce8ccc9caec9fec1b019a31f931725ca94;
+
+    constructor(string _templateName) public {
+        issuer = PAIIssuer(flow.deployContract(1, _templateName, ""));
+    }
+
+    function testCreate() public {
+        issuer.init("pai-issuer");
+
+        issuer.mint(100000000, dest);
+        assertEq(100000000, flow.balance(dest, issuer.getAssetType()));
+    }
+
+    function testCreateAndMint() public {
+        issuer.init("pai-issuer");
+
+        issuer.mint(100000000, dest);
+        issuer.mint(100000000, dest);
+        assertEq(200000000, flow.balance(dest,issuer.getAssetType()));
+    }
+
+}
---
+++ pai-experimental/testcase/price_oracle.tc.sol
@@ -0,0 +1,27 @@
+pragma solidity 0.4.25;
+
+import "../../library/template.sol";
+import "../price_oracle.sol";
+import "../3rd/test.sol";
+
+// import "github.com/seeplayerone/dapp-bin/library/template.sol";
+// import "github.com/seeplayerone/dapp-bin/pai-experimental/price_oracle.sol"
+// import "github.com/seeplayerone/dapp-bin/pai-experimental/3rd/test.sol";
+
+contract PriceOracleTest is Template, DSTest {
+    PriceOracle private oracle;
+
+    constructor(string _templateName) public {
+        oracle =  PriceOracle(flow.deployContract(1, _templateName, ""));        
+    }
+
+    function testUpdatePriceSuccess() public {
+        oracle.updatePrice(0, 88);
+        assertEq(88, oracle.getPrice(0));
+    }
+
+    function testUpdatePriceFail() public {
+        oracle.updatePrice(0, 88);
+        assertEq(77, oracle.getPrice(0));
+    }
+}
