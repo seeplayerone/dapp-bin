9307c0a39ec125a3d96897f60bdccab11398ada6
jackyxxu <xxd@ipaitao.com>
2019-08-14 14:28:13 +0800

    update liquidator.tc.sol

--- pai-experimental/testcase/liquidator.tc.sol
+++ pai-experimental/testcase/liquidator.tc.sol
@@ -10,10 +10,16 @@ import "github.com/seeplayerone/dapp-bin/library/template.sol";
 import "github.com/seeplayerone/dapp-bin/pai-experimental/liquidator.sol";
 import "github.com/seeplayerone/dapp-bin/pai-experimental/3rd/test.sol";
 
+contract FakeTemplateNamePAIIssuer is PAIIssuer {
+    constructor() public {
+        templateName = "Fake-Template-Name-For-Test";
+    }
+}
+
 contract LiquidatorTest is Template, DSTest, DSMath {
     Liquidator private liquidator;
     PriceOracle private oracle;
-    PAIIssuer private issuer;
+    FakeTemplateNamePAIIssuer private issuer;
 
     uint private discount = 970000000000000000000000000;
     uint private ASSET_BTC;
@@ -23,13 +29,23 @@ contract LiquidatorTest is Template, DSTest, DSMath {
 
     }
 
-    constructor(address liq, address ora, address iss) public {
-        liquidator = Liquidator(liq);
-        oracle = PriceOracle(ora);
-        issuer = PAIIssuer(iss);
+    constructor() public payable {
+        /// Please send me 100 BTC
+    }
 
+    function setup() public {
         ASSET_BTC = 0; /// using ASIM asset for test purpose
+        
+        oracle = new PriceOracle();
+        oracle.updatePrice(ASSET_BTC, RAY * 10);
+
+        issuer = new FakeTemplateNamePAIIssuer();
+        issuer.init("sb");
         ASSET_PAI = issuer.getAssetType();
+
+        liquidator = new Liquidator(oracle, issuer);
+
+        issuer.mint(100000000000, this);
     }
 
     function debug() public view returns(uint) {
@@ -37,23 +53,27 @@ contract LiquidatorTest is Template, DSTest, DSMath {
     }
 
     function testAddDebt() public {
+        setup();
         liquidator.addDebt(100000000);
         assertEq(100000000, liquidator.totalDebtPAI());
     }
 
     function testAddPAI() public {
+        setup();
         uint value = 1000000000;
         liquidator.addPAI.value(value, ASSET_PAI)();
         assertEq(value, liquidator.totalAssetPAI());
     }
 
     function testAddBTC() public {
+        setup();
         uint value = 1000000000;
         liquidator.addBTC.value(value, ASSET_BTC)();
         assertEq(value, liquidator.totalCollateralBTC());
     }
 
     function testCancelDebtWithPAIRemaining() public {
+        setup();
         uint value = 1000000000;
         liquidator.addPAI.value(value, ASSET_PAI)();
         liquidator.addDebt(value/2);
@@ -61,6 +81,7 @@ contract LiquidatorTest is Template, DSTest, DSMath {
     }
 
     function testCancelDebtWithDebtRemaining() public {
+        setup();
         uint value = 1000000000;
         liquidator.addPAI.value(value, ASSET_PAI)();
         liquidator.addDebt(value*2);
@@ -68,6 +89,7 @@ contract LiquidatorTest is Template, DSTest, DSMath {
     }
 
     function testAddDebtAndBTC() public {
+        setup();
         uint value = 1000000000;
         liquidator.addBTC.value(value, ASSET_BTC)();
         assertEq(value, liquidator.totalCollateralBTC());
@@ -76,6 +98,7 @@ contract LiquidatorTest is Template, DSTest, DSMath {
     }
 
     function testCollateralPrice() public {
+        setup();
         oracle.updatePrice(0, 10*(10**27));
         assertEq(10*(10**27), liquidator.collateralPrice());
     }
@@ -83,9 +106,8 @@ contract LiquidatorTest is Template, DSTest, DSMath {
     //// should be tested when there is BTC in Liquidator
     //// let's say 10 BTC
     function testBuyCollateralNormal() public {
-
+        setup();
         liquidator.addBTC.value(1000000000, ASSET_BTC)();
-
         assertEq(1000000000, liquidator.totalCollateralBTC());
 
         oracle.updatePrice(0, 10*(10**27));
@@ -110,6 +132,7 @@ contract LiquidatorTest is Template, DSTest, DSMath {
     //// should be tested when there is BTC and -PAI in Liquidator
     //// let's say 10 BTC and -500 PAI
     function testBuyCollateralSettlement() public {
+        setup();
         liquidator.addBTC.value(1000000000, ASSET_BTC)();
         liquidator.addDebt(50000000000);
 
