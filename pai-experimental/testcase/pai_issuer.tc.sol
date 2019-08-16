9428102e756fe6571f02619bf0fb2acd28de764f
jackyxxu <xxd@ipaitao.com>
2019-08-16 17:24:37 +0800

    update pai_issuer.tc.sol

--- pai-experimental/testcase/pai_issuer.tc.sol
+++ pai-experimental/testcase/pai_issuer.tc.sol
@@ -8,18 +8,21 @@ import "github.com/seeplayerone/dapp-bin/library/template.sol";
 import "github.com/seeplayerone/dapp-bin/pai-experimental/pai_issuer.sol";
 import "github.com/seeplayerone/dapp-bin/pai-experimental/3rd/test.sol";
 
-contract FakeTemplateNamePAIIssuer is PAIIssuer {
+contract FakePAIIssuer is PAIIssuer {
     constructor() public {
         templateName = "Fake-Template-Name-For-Test";
     }
 }
 
 contract PAIIssuerTest is Template, DSTest {
-    FakeTemplateNamePAIIssuer private issuer;
+    FakePAIIssuer private issuer;
     address private dest = 0x668eb397ce8ccc9caec9fec1b019a31f931725ca94;
+    address private hole = 0x660000000000000000000000000000000000000000;
+
+    function() public payable {}
 
     function setup() public {
-        issuer = new FakeTemplateNamePAIIssuer();
+        issuer = new FakePAIIssuer();
         issuer.init("sb");
     }
 
@@ -35,5 +38,15 @@ contract PAIIssuerTest is Template, DSTest {
         issuer.mint(100000000, dest);
         assertEq(200000000, flow.balance(dest,issuer.getAssetType()));
     }
+    
+    function testCreateBurn() public {
+        setup();
+        issuer.mint(200000000, this);
 
+        assertEq(200000000, flow.balance(this, issuer.getAssetType()));
+        issuer.burn.value(100000000, issuer.getAssetType())();
+
+        assertEq(100000000, flow.balance(this, issuer.getAssetType()));
+        assertEq(100000000, flow.balance(hole, issuer.getAssetType()));
+    }
 }
