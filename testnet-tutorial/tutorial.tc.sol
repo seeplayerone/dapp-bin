9b271e37b7915f92975bfcbfe8926f9db3746266
jackyxxu <xxd@ipaitao.com>
2019-08-27 11:10:33 +0800

    pass test

--- testnet-tutorial/tutorial.tc.sol
+++ testnet-tutorial/tutorial.tc.sol
@@ -11,9 +11,12 @@ contract NamedTutorial is Tutorial {
 
 contract TutorialTest is DSTest {
     NamedTutorial private tutorial;
-    address private dest = 0x668eb397ce8ccc9caec9fec1b019a31f931725ca94;
 
     uint private SATOSHI = 10**8;
+
+    function() public payable {
+
+    }
 
     function test() public returns (bool) {
         tutorial = new NamedTutorial();
@@ -24,13 +27,13 @@ contract TutorialTest is DSTest {
         tutorial.mint(10 * SATOSHI);
         assertEq(tutorial.checkBalance(), 20 * SATOSHI);
 
-        tutorial.transfer(dest, 10 * SATOSHI);
+        tutorial.transfer(this, 10 * SATOSHI);
         assertEq(tutorial.checkBalance(), 10 * SATOSHI);
         assertEq(tutorial.checkTotalSupply(), 20 * SATOSHI);
 
-        // tutorial.burn.value(10 * SATOSHI, assettype)();
-        // assertEq(tutorial.checkBalance(), 10 * SATOSHI);
-        // assertEq(tutorial.checkTotalSupply(), 10 * SATOSHI);
+        tutorial.burn.value(10 * SATOSHI, assettype)();
+        assertEq(tutorial.checkBalance(), 10 * SATOSHI);
+        assertEq(tutorial.checkTotalSupply(), 10 * SATOSHI);
 
         return true;
     }
