35aa6f462bd15992ffdaf378e49e005719936de9
macong <13764266649@163.com>
2019-09-04 09:45:34 +0800

    testsave

--- pai-experimental/pai_issuer.sol
+++ pai-experimental/pai_issuer.sol
@@ -5,13 +5,7 @@ pragma solidity 0.4.25;
 
 import "github.com/evilcc2018/dapp-bin/library/template.sol";
 import "github.com/evilcc2018/dapp-bin/pai-experimental/3rd/math.sol";
-
-
-/// @dev the Registry interface
-///  Registry is a system contract, an organization needs to register before issuing assets
-interface RegistryKuKu {
-     function registerOrganization(string organizationName, string templateName) external returns(uint32);
-}
+import "github.com/evilcc2018/dapp-bin/pai-experimental/registeryInterface.sol";
 
 contract PAIIssuer is Template, DSMath {
     string private name = "PAI_ISSUER";
@@ -32,7 +26,7 @@ contract PAIIssuer is Template, DSMath {
     function init(string _name) public {
         name = _name;
         /// TODO organization registration should be done in DAO
-        RegistryKuKu registry = RegistryKuKu(0x630000000000000000000000000000000000000065);
+        Registry registry = Registry(0x630000000000000000000000000000000000000065);
         orgnizationID = registry.registerOrganization(name, templateName);
         assetIndex = 1;
         assetType = 0;
