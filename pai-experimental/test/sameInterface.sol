c42fe89a5d1a0936bda7c0e86f1f6ab1ac7b23cf
macong <13764266649@163.com>
2019-09-04 11:32:15 +0800

    test

---
+++ pai-experimental/test/sameInterface.sol
@@ -0,0 +1,26 @@
+pragma solidity 0.4.25;
+
+import "github.com/evilcc2018/dapp-bin/pai-experimental/test/org1.sol";
+import "github.com/evilcc2018/dapp-bin/pai-experimental/test/org2.sol";
+
+
+contract FakeORG1 is ORG1 {
+    constructor() public {
+        templateName = "ORG1";
+    }
+}
+
+contract FakeORG2 is ORG2 {
+    constructor() public {
+        templateName = "ORG2";
+    }
+}
+
+contract TestInterface is Template {
+    function init() public {
+        FakeORG1 o1 = new FakeORG1();
+        o1.init("o1");
+        FakeORG2 o2 = new FakeORG1();
+        o2.init("o2");
+    }
+}
