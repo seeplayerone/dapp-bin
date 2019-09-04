f63c13ab3d1dc99a8d3bd85c2b4b0cfd947140a0
macong <13764266649@163.com>
2019-09-04 09:20:34 +0800

    change path

--- pai-experimental/test/testlog2.sol
+++ pai-experimental/test/testlog2.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.4.25;
 
-import "github.com/evilcc2018/dapp-bin/pai-experimental/mathPI.sol";
-import "github.com/evilcc2018/dapp-bin/pai-experimental/testPI.sol";
+import "github.com/seeplayerone/dapp-bin/pai-experimental/mathPI.sol";
+import "github.com/seeplayerone/dapp-bin/pai-experimental/testPI.sol";
 
 
 contract TestMath is DSTest, MathPI {
