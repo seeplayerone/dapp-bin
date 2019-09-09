dee53d20b1afe6e5fb6689b3e61f1d0bbba28000
macong <13764266649@163.com>
2019-09-09 20:36:02 +0800

    save

--- pai-experimental/testcase/pai_issuer.tc.sol
+++ pai-experimental/testcase/pai_issuer.tc.sol
@@ -5,13 +5,7 @@ import "github.com/evilcc2018/dapp-bin/pai-experimental/pai_issuer.sol";
 import "github.com/evilcc2018/dapp-bin/pai-experimental/testPI.sol";
 import "github.com/evilcc2018/dapp-bin/pai-experimental/pai_main.sol";
 
-contract FakePerson is Template {
-    function() public payable {}
-
-    // function createPAIDAO(string _str) public returns (address) {
-    //     return (new FakePaiDao(_str));
-    // }
-}
+import "./fakeperson.sol";
 
 contract FakePAIIssuer is PAIIssuer {
     constructor(string _organizationName, address paiMainContract)
@@ -70,27 +64,54 @@ contract TestCase is Template, DSTest {
         assertEq(200000000,flow.balance(p1,ASSET_PAI));//1
     }
 
-    // function testCreate() public {
-    //     setup();
-    //     issuer.mint(100000000, dest);
-    //     assertEq(100000000, flow.balance(dest, issuer.getAssetType()));
-    // }
+    function testAssetRelated() public {
+        FakePaiDaoNoGovernance paiDAO = new FakePaiDaoNoGovernance("PAIDAO");
+        paiDAO.init();
+        ASSET_PIS = paiDAO.PISGlobalId();
+        FakePAIIssuer issuer = new FakePAIIssuer("PAIISSUER",paiDAO);
+        issuer.init();
+        ASSET_PAI = issuer.PAIGlobalId();
 
-    // function testCreateAndMint() public {
-    //     setup();
-    //     issuer.mint(100000000, dest);
-    //     issuer.mint(100000000, dest);
-    //     assertEq(200000000, flow.balance(dest,issuer.getAssetType()));
-    // }
-    
-    // function testCreateBurn() public {
-    //     setup();
-    //     issuer.mint(200000000, this);
+        FakePerson p1 = new FakePerson();
+        p1.callMint(issuer,100000000,p1);
+        (bool exist, string memory name, string memory symbol, string memory description, uint32 assetType, uint totalSupply) =
+            issuer.getAssetInfo(0);
+        assertTrue(exist);//0
+        assertEq(name,"PAI");//1
+        assertEq(symbol,"PAI");//2
+        assertEq(description,"PAI Stable Coin");//3
+        assertEq(uint(assetType),0);//4
+        assertEq(totalSupply,100000000);//5
 
-    //     assertEq(200000000, flow.balance(this, issuer.getAssetType()));
-    //     issuer.burn.value(100000000, issuer.getAssetType())();
+        issuer.mint(100000000,p1);
+        (,,,,,totalSupply) = issuer.getAssetInfo(0);
+        assertEq(totalSupply,200000000);//6
+        bool tempBool = p1.callBurn(issuer,50000000,ASSET_PAI);
+        assertTrue(tempBool);//7
+        (,,,,,totalSupply) = issuer.getAssetInfo(0);
+        assertEq(totalSupply,150000000);//8
+    }
 
-    //     assertEq(100000000, flow.balance(this, issuer.getAssetType()));
-    //     assertEq(100000000, flow.balance(hole, issuer.getAssetType()));
-    // }
+    function testGovernance() public {
+        FakePaiDao paiDAO;
+        FakePerson p1 = new FakePerson();
+        FakePerson p2 = new FakePerson();
+        // FakePerson p4 = new FakePerson();
+        // FakePerson p5 = new FakePerson();
+
+        paiDAO = FakePaiDao(p1.createPAIDAO("PAIDAO"));
+        paiDAO.init();
+        ASSET_PIS = paiDAO.PISGlobalId();
+        FakePAIIssuer issuer = new FakePAIIssuer("PAIISSUER",paiDAO);
+        issuer.init();
+        ASSET_PAI = issuer.PAIGlobalId();
+
+        bool tempBool = p1.callMint(issuer,100000000,p1);
+        assertTrue(tempBool);
+        tempBool = p2.callMint(issuer,100000000,p1);
+        assertTrue(!tempBool);
+
+
+
+    }
 }
--- pai-experimental/testcase/pai_main.tc.sol
+++ pai-experimental/testcase/pai_main.tc.sol
@@ -5,67 +5,8 @@ import "github.com/evilcc2018/dapp-bin/pai-experimental/3rd/math.sol";
 import "github.com/evilcc2018/dapp-bin/pai-experimental/testPI.sol";
 import "github.com/evilcc2018/dapp-bin/pai-experimental/pai_main.sol";
 
-contract FakePerson is Template {
-    function() public payable {}
+import "./fakeperson.sol";
 
-    function createPAIDAO(string _str) public returns (address) {
-        return (new FakePaiDao(_str));
-    }
-
-    function createPAIDAONoGovernance(string _str) public returns (address) {
-        return (new FakePaiDaoNoGovernance(_str));
-    }
-
-    function callInit(address paidao) public returns (bool) {
-        bytes4 methodId = bytes4(keccak256("init()"));
-        bool result = PAIDAO(paidao).call(methodId);
-        return result;
-    }
-
-    function callCreateNewRole(address paidao, string newRole, string superior) public returns (bool) {
-        bytes4 methodId = bytes4(keccak256("createNewRole(bytes,bytes)"));
-        bool result = PAIDAO(paidao).call(abi.encodeWithSelector(methodId,bytes(newRole),bytes(superior)));
-        return result;
-    }
-
-    function callAddMember(address paidao, address _address, string role) public returns (bool) {
-        bytes4 methodId = bytes4(keccak256("addMember(address,bytes)"));
-        bool result = PAIDAO(paidao).call(abi.encodeWithSelector(methodId,_address,bytes(role)));
-        return result;
-    }
-
-    function callRemoveMember(address paidao, address _address, string role) public returns (bool) {
-        bytes4 methodId = bytes4(keccak256("removeMember(address,bytes)"));
-        bool result = PAIDAO(paidao).call(abi.encodeWithSelector(methodId,_address,bytes(role)));
-        return result;
-    }
-
-    function callMint(address paidao, uint amount, address dest) public returns (bool) {
-        bytes4 methodId = bytes4(keccak256("mint(uint256,address)"));
-        bool result = PAIDAO(paidao).call(abi.encodeWithSelector(methodId,amount,dest));
-        return result;
-    }
-
-    function callBurn(address paidao, uint amount, uint96 id) public returns (bool) {
-        bytes4 methodId = bytes4(keccak256("burn()"));
-        bool result = PAIDAO(paidao).call.value(amount,id)(abi.encodeWithSelector(methodId));
-        return result;
-    }
-
-    function callResetMembers(address paidao, address[] _members, string role) public returns (bool) {
-        bytes4 methodId = bytes4(keccak256("resetMembers(address[],bytes)"));
-        bool result = PAIDAO(paidao).call(abi.encodeWithSelector(methodId, _members, bytes(role)));
-        return result;
-    }
-
-    function callChangeSuperior(address paidao, string role, string newSuperior) public returns (bool) {
-        bytes4 methodId = bytes4(keccak256("changeSuperior(bytes,bytes)"));
-        bool result = PAIDAO(paidao).call(abi.encodeWithSelector(methodId, bytes(role),bytes(newSuperior)));
-        return result;
-    }
-
-    
-}
 
 contract FakePaiDao is PAIDAO {
     constructor(string _organizationName)
