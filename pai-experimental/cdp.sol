7b908e6a5a773c09d734d01f806d7f6eeb72bde0
macong <13764266649@163.com>
2019-09-16 11:41:28 +0800

    save

--- pai-experimental/cdp.sol
+++ pai-experimental/cdp.sol
@@ -334,9 +334,9 @@ contract CDP is MathPI, DSNote, Template, ACLSlave {
         require(setting.globalOpen());
         require(!disableALLCDPFunction);
         require(!disableCDPCreation);
-        // require(mul(msg.value, priceOracle.getPrice()) / amount >= sub(createCollateralRatio,createRatioTolerance));
-        // require(amount >= lowerBorrowingLimit);
-        // require(add(msg.value,totalCollateral()) <= debtCeiling);
+        require(mul(msg.value, priceOracle.getPrice()) / amount >= sub(createCollateralRatio,createRatioTolerance));
+        require(amount >= lowerBorrowingLimit);
+        require(add(msg.value,totalCollateral()) <= debtCeiling);
         // (,,,,,uint totalPaiSupply) = issuer.getAssetInfo(0);
         // require(add(totalPrincipal,amount) <= rmul(totalPaiSupply, debtRateCeiling) || 0 == totalPaiSupply);
         uint id = createCDPInternal(_type);
