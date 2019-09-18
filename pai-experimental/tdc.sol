881295b0873b4ccad37b1d987bc012be42f26654
macong <13764266649@163.com>
2019-09-18 17:43:21 +0800

    save

--- pai-experimental/tdc.sol
+++ pai-experimental/tdc.sol
@@ -199,7 +199,7 @@ contract TDC is DSMath, DSNote, Template, ACLSlave {
             TDCRecords[record].owner.transfer(principal,ASSET_PAI);
         }
         TDCRecords[record].principal = 0;
-        //finance.payForInterest(167,TDCRecords[record].owner);
+        finance.payForInterest(167,TDCRecords[record].owner);
         emit ReturnMoney(record,TDCRecords[record].owner,principal,interest);
     }
 }
