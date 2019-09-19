0d3d84c53d29ec0f366570ad23b832f11ee12406
jackyxxu <xxd@ipaitao.com>
2019-09-19 14:33:55 +0800

    make quicksort memory in election.sol

--- library/election.sol
+++ library/election.sol
@@ -148,8 +148,8 @@ contract Election is Template {
         require(election.candidates.length == election.candidateSupportRates.length);
 
         if(election.candidates.length > 1) {
-            (election.candidateSupportRates, election.candidates) = bubbleSort(election.candidateSupportRates, election.candidates);
-            // quickSort(election.candidateSupportRates, election.candidates, int(0), int(election.candidates.length - 1));
+            // (election.candidateSupportRates, election.candidates) = bubbleSort(election.candidateSupportRates, election.candidates);
+            (election.candidateSupportRates, election.candidates) = quickSort(election.candidateSupportRates, election.candidates, int(0), int(election.candidates.length - 1));
         }
 
         election.sorted = true;
@@ -182,25 +182,27 @@ contract Election is Template {
     }
 
     /// @dev https://gist.github.com/subhodi/b3b86cc13ad2636420963e692a4d896f
-    function quickSort(uint[] storage arr, address[] storage addresses, int left, int right) internal{
+    function quickSort(uint[] memory values, address[] memory addresses, int left, int right) internal returns (uint[], address[]){
         int i = left;
         int j = right;
         if(i==j) return;
-        uint pivot = arr[uint(left + (right - left) / 2)];
+        uint pivot = values[uint(left + (right - left) / 2)];
         while (i <= j) {
-            while (arr[uint(i)] > pivot) i++;
-            while (pivot > arr[uint(j)]) j--;
+            while (values[uint(i)] > pivot) i++;
+            while (pivot > values[uint(j)]) j--;
             if (i <= j) {
-                (arr[uint(i)], arr[uint(j)]) = (arr[uint(j)], arr[uint(i)]);
+                (values[uint(i)], values[uint(j)]) = (values[uint(j)], values[uint(i)]);
                 (addresses[uint(i)], addresses[uint(j)]) = (addresses[uint(j)], addresses[uint(i)]);                
                 i++;
                 j--;
             }
         }
         if (left < j)
-            quickSort(arr, addresses, left, j);
+            quickSort(values, addresses, left, j);
         if (i < right)
-            quickSort(arr, addresses, i, right);
+            quickSort(values, addresses, i, right);
+        
+        return (values, addresses);
     }
 
     /// @dev readonly functions
