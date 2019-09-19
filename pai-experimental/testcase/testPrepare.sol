3b77e857dc465cdd2f0bf3ff2f1390eb21cc635d
macong <13764266649@163.com>
2019-09-19 14:43:24 +0800

    save

--- pai-experimental/testcase/testPrepare.sol
+++ pai-experimental/testcase/testPrepare.sol
@@ -386,7 +386,7 @@ contract FakePerson is Template {
     }
 
     function callCashOut(address finance, uint amount, address dest) public returns (bool) {
-        bytes4 methodId = bytes4(keccak256("cashOut(bool)"));
+        bytes4 methodId = bytes4(keccak256("cashOut(uint256,address)"));
         bool result = TimefliesFinance(finance).call(abi.encodeWithSelector(methodId,amount,dest));
         return result;
     }
