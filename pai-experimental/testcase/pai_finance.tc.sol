b23ab4b343c624a2fb2a39ffaa3ae7964cf01a3a
macong <13764266649@163.com>
2019-09-19 19:55:41 +0800

    save

--- pai-experimental/testcase/pai_finance.tc.sol
+++ pai-experimental/testcase/pai_finance.tc.sol
@@ -71,10 +71,10 @@ contract TestBase is Template, DSTest, DSMath {
         bool tempBool;
         btcIssuer.mint(200000000000, p1);
         p1.callCreateDepositBorrow(cdp,1000000000,0,2000000000,ASSET_BTC);
-        cdp.fly(1 years);
+        cdp.fly(365 days);
         tempBool = p1.callCreateDepositBorrow(cdp,1000000000,0,2000000000,ASSET_BTC);
         assertTrue(tempBool); //0
-        cdp.fly(1);
+        cdp.fly(1 days);
         tempBool = p1.callCreateDepositBorrow(cdp,1000000000,0,2000000000,ASSET_BTC);
         assertTrue(tempBool); //1
         cdp.fly(1 days);
@@ -90,7 +90,7 @@ contract TestBase is Template, DSTest, DSMath {
         assertTrue(tempBool);//3
 
         assertEq(cdp.accumulatedRates(),0);
-       
+        assertEq(cdp.updateAndFetchRates(),0);
         btcIssuer.mint(200000000000, p2);
 
     }
