d075ff1cae66a8724501cbc1ef9c1f39bd453df6
macong <13764266649@163.com>
2019-09-20 10:09:30 +0800

    save

--- pai-experimental/cdp.sol
+++ pai-experimental/cdp.sol
@@ -319,12 +319,12 @@ contract CDP is MathPI, DSNote, Template, ACLSlave {
             data.accumulatedDebt = add(data.accumulatedDebt, newDebt);
             emit BorrowPAI(data.collateral, data.principal,rmul(data.accumulatedDebt, accumulatedRates), record, amount,0);
         } else {
-            // newDebt = add(amount,rmul(amount, mul(getInterestRate(data.cdpType), term[uint8(data.cdpType)])) / 1 years);
-            // data.accumulatedDebt = add(data.accumulatedDebt, newDebt);
-            // data.endTime = add(timeNow(), term[uint8(data.cdpType)]);
-            // emit BorrowPAI(data.collateral, data.principal, data.accumulatedDebt, record, amount,data.endTime);
+            newDebt = add(amount,rmul(amount, mul(getInterestRate(data.cdpType), term[uint8(data.cdpType)])) / 1 years);
+            data.accumulatedDebt = add(data.accumulatedDebt, newDebt);
+            data.endTime = add(timeNow(), term[uint8(data.cdpType)]);
+            emit BorrowPAI(data.collateral, data.principal, data.accumulatedDebt, record, amount,data.endTime);
         }
-        require(safe(record));
+        //require(safe(record));
 
         issuer.mint(amount, msg.sender);
     }
@@ -469,17 +469,16 @@ contract CDP is MathPI, DSNote, Template, ACLSlave {
     }
 
     function safe(uint record) public returns (bool) {
-        // CDPRecord storage data = CDPRecords[record];
-        // require(data.owner != 0x0);
-        // // if(CDPType.CURRENT != data.cdpType && add(data.endTime,overdueBufferPeriod) < timeNow()) {
-        // //     return false;
-        // // }
+        CDPRecord storage data = CDPRecords[record];
+        require(data.owner != 0x0);
+        if(CDPType.CURRENT != data.cdpType && add(data.endTime,overdueBufferPeriod) < timeNow()) {
+            return false;
+        }
 
-        // uint256 collateralValue = rmul(data.collateral, priceOracle.getPrice());
-        // (uint principal,uint interest) = debtOfCDP(record);
-        // uint256 debtValue = rmul(add(principal,interest), liquidationRatio);
-        // return collateralValue >= debtValue;
-        return true;
+        uint256 collateralValue = rmul(data.collateral, priceOracle.getPrice());
+        (uint principal,uint interest) = debtOfCDP(record);
+        uint256 debtValue = rmul(add(principal,interest), liquidationRatio);
+        return collateralValue >= debtValue;
     }
 
     function updateAndFetchRates() public returns (uint256) {
