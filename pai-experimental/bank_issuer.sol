a225d79885a0c57191f609f0e334474d18d41f69
macong <13764266649@163.com>
2019-10-09 11:16:52 +0800

    save

--- pai-experimental/bank_issuer.sol
+++ pai-experimental/bank_issuer.sol
@@ -41,19 +41,20 @@ contract BankIssuer is Template, Asset, DSMath, ACLSlave {
      * @param symbol asset symbol
      * @param description asset description
      */
-    function createAsset(string name, string symbol, string description) public auth("DirectorVote@Bank")
+    function createAsset(string name, string symbol, string description) public 
+    //auth("DirectorVote@Bank")
     {
         require(bytes(name).length > 0, "asset requires a name");
         require(bytes(symbol).length > 0, "asset requires a symbol");
         assetIndex = assetIndex + 1;
         require(assetIndex != 0, "assetIndex has overflowed");
-        newAsset(name, symbol, description, 0, assetIndex, 1000);
-        flow.createAsset(0, assetIndex, 1000);
-        zeroAddr.transfer(1000, AssetGlobalId[assetIndex]);
-        issuedAssets[assetIndex].totalIssued = 0;
-        uint64 assetId = uint64(0) << 32 | uint64(organizationId);
-        AssetGlobalId[assetIndex] = uint96(assetId) << 32 | uint96(assetIndex);
-        emit CreateAsset(bytes12(AssetGlobalId[assetIndex]));
+        // newAsset(name, symbol, description, 0, assetIndex, 1000);
+        // flow.createAsset(0, assetIndex, 1000);
+        // zeroAddr.transfer(1000, AssetGlobalId[assetIndex]);
+        // issuedAssets[assetIndex].totalIssued = 0;
+        // uint64 assetId = uint64(0) << 32 | uint64(organizationId);
+        // AssetGlobalId[assetIndex] = uint96(assetId) << 32 | uint96(assetIndex);
+        // emit CreateAsset(bytes12(AssetGlobalId[assetIndex]));
     }
 
     function mint(uint32 assetIndex, uint amount, address dest) public auth("BusinessContract@Bank") {
