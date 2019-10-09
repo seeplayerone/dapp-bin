d5e5182ec5d1905f6d9d43631f8c1d41cd43f545
macong <13764266649@163.com>
2019-10-09 10:52:08 +0800

    save

--- pai-experimental/bank_issuer.sol
+++ pai-experimental/bank_issuer.sol
@@ -13,7 +13,10 @@ contract BankIssuer is Template, Asset, DSMath, ACLSlave {
     bool private registed = false;
 
     mapping(uint => uint96) AssetGlobalId;
-    uint public assetIndex = 0;
+    uint32 public assetIndex = 0; //asset index in the organization
+
+    /// crate asset
+    event CreateAsset(bytes12);
 
     ///params for burn
     address private constant zeroAddr = 0x660000000000000000000000000000000000000000;
@@ -37,38 +40,34 @@ contract BankIssuer is Template, Asset, DSMath, ACLSlave {
      * @param name asset name
      * @param symbol asset symbol
      * @param description asset description
-     * @param assetType asset type, DIVISIBLE + ANONYMOUS + RESTRICTED
-     * @param assetIndex asset index in the organization
-     * @param amountOrVoucherId amount or voucherId of asset
      */
-    function createAsset(string name, string symbol, string description)
-    public
+    function createAsset(string name, string symbol, string description) public auth("DirectorVote@Bank")
     {
         require(bytes(name).length > 0, "asset requires a name");
         require(bytes(symbol).length > 0, "asset requires a symbol");
-        assetIndex = add(assetIndex,1)
-
+        assetIndex = assetIndex + 1;
+        require(assetIndex != 0, "assetIndex has overflowed");
         newAsset(name, symbol, description, 0, assetIndex, 1);
-
+        flow.createAsset(0, assetIndex, 1);
+        zeroAddr.transfer(1, AssetGlobalId[assetIndex]);
+        issuedAssets[assetIndex].totalIssued = 0;
         uint64 assetId = uint64(0) << 32 | uint64(organizationId);
         AssetGlobalId[assetIndex] = uint96(assetId) << 32 | uint96(assetIndex);
-
-        zeroAddr.transfer(1, PAIGlobalId);
         emit CreateAsset(bytes12(AssetGlobalId[assetIndex]));
     }
 
-    function mint(uint assetIndex, uint amount, address dest) public {
+    function mint(uint32 assetIndex, uint amount, address dest) public auth("BusinessContract@Bank") {
         if(!issuedAssets[assetIndex].existed) {
             return;
-        } 
+        }
         flow.mintAsset(assetIndex, amount);
         updateAsset(assetIndex, amount);
         dest.transfer(amount, AssetGlobalId[assetIndex]);
     }
 
-    function burn(uint assetIndex) public payable {
+    function burn(uint32 assetIndex) public payable {
         require(msg.assettype == AssetGlobalId[assetIndex],"index and asset not match");
-        issuedAssets[index].totalIssued = sub(issuedAssets[index].totalIssued, msg.value);
-        zeroAddr.transfer(msg.value, PAIGlobalId);
+        issuedAssets[assetIndex].totalIssued = sub(issuedAssets[assetIndex].totalIssued, msg.value);
+        zeroAddr.transfer(msg.value, AssetGlobalId[assetIndex]);
     }
 }
---
+++ pai-experimental/testcase/bank_issuer.tc.sol
@@ -0,0 +1,87 @@
+pragma solidity 0.4.25;
+
+import "github.com/evilcc2018/dapp-bin/library/template.sol";
+import "github.com/evilcc2018/dapp-bin/pai-experimental/bank_issuer.sol";
+import "github.com/evilcc2018/dapp-bin/pai-experimental/testPI.sol";
+import "github.com/evilcc2018/dapp-bin/pai-experimental/pai_main.sol";
+import "github.com/evilcc2018/dapp-bin/pai-experimental/testcase/testPrepare.sol";
+
+
+
+contract TestBankIssuer is Template, DSTest {
+    uint96 ASSET_PIS;
+    uint96 ASSET_PAI;
+    function() public payable {}
+
+    function testInit() public {
+        FakePaiDaoNoGovernance paiDAO = new FakePaiDaoNoGovernance("PAIDAO");
+        paiDAO.init();
+        FakePAIIssuer issuer = new FakePAIIssuer("PAIISSUER",paiDAO);
+        issuer.init();
+
+        FakePerson p1 = new FakePerson();
+
+        ASSET_PIS = paiDAO.PISGlobalId();
+        ASSET_PAI = issuer.PAIGlobalId();
+
+        paiDAO.mint(100000000,p1);
+        assertEq(100000000,flow.balance(p1,ASSET_PIS));//0
+
+        issuer.mint(200000000,p1);
+        assertEq(200000000,flow.balance(p1,ASSET_PAI));//1
+    }
+
+    function testAssetRelated() public {
+        FakePaiDaoNoGovernance paiDAO = new FakePaiDaoNoGovernance("PAIDAO");
+        paiDAO.init();
+        ASSET_PIS = paiDAO.PISGlobalId();
+        FakePAIIssuer issuer = new FakePAIIssuer("PAIISSUER",paiDAO);
+        issuer.init();
+        ASSET_PAI = issuer.PAIGlobalId();
+
+        FakePerson p1 = new FakePerson();
+        p1.callMint(issuer,100000000,p1);
+        (bool exist, string memory name, string memory symbol, string memory description, uint32 assetType, uint totalSupply) =
+            issuer.getAssetInfo(0);
+        assertTrue(exist);//0
+        assertEq(name,"PAI");//1
+        assertEq(symbol,"PAI");//2
+        assertEq(description,"PAI Stable Coin");//3
+        assertEq(uint(assetType),0);//4
+        assertEq(totalSupply,100000000);//5
+
+        issuer.mint(100000000,p1);
+        (,,,,,totalSupply) = issuer.getAssetInfo(0);
+        assertEq(totalSupply,200000000);//6
+        bool tempBool = p1.callBurn(issuer,50000000,ASSET_PAI);
+        assertTrue(tempBool);//7
+        (,,,,,totalSupply) = issuer.getAssetInfo(0);
+        assertEq(totalSupply,150000000);//8
+    }
+
+    function testGovernance() public {
+        FakePaiDao paiDAO;
+        FakePerson p1 = new FakePerson();
+        FakePerson p2 = new FakePerson();
+
+        paiDAO = FakePaiDao(p1.createPAIDAO("PAIDAO"));
+        paiDAO.init();
+        ASSET_PIS = paiDAO.PISGlobalId();
+        FakePAIIssuer issuer = new FakePAIIssuer("PAIISSUER",paiDAO);
+        issuer.init();
+        ASSET_PAI = issuer.PAIGlobalId();
+
+        bool tempBool = p1.callMint(issuer,100000000,p1);
+        assertTrue(!tempBool);//0
+        tempBool = p2.callMint(issuer,100000000,p1);
+        assertTrue(!tempBool);//1
+        tempBool = p1.callCreateNewRole(paiDAO,"PAIMINTER","ADMIN",0);
+        assertTrue(tempBool);//2
+        tempBool = p1.callAddMember(paiDAO,p2,"PAIMINTER");
+        assertTrue(tempBool);//3
+        tempBool = p1.callMint(issuer,100000000,p1);
+        assertTrue(!tempBool);//4
+        tempBool = p2.callMint(issuer,100000000,p1);
+        assertTrue(tempBool);//5
+    }
+}
