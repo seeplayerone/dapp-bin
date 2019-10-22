00b936c1c5bd16019161c98293ba901a1e0f5443
chendeheng <18815286346@163.com>
2019-10-22 14:24:28 +0800

    fix interface func bug

--- library/test-registry/organization.sol
+++ library/test-registry/organization.sol
@@ -10,7 +10,7 @@ interface Registry {
      function registerOrganization(string organizationName, string templateName) external returns(uint32);
      function renameOrganization(string organizationName) external;
      function newAsset(string name, string symbol, string description, uint32 assetType, uint32 assetIndex) external;
-     function updateAsset(assetIndex, amountOrVoucherId) external;
+     function updateAsset(uint32 assetIndex, uint amountOrVoucherId) external;
      function updateOrganizationStatus(bool status) external;
 }
 
